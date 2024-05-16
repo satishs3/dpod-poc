69678649ffe9db8193552dfc58f5587791e7eb65
Shivam <shivamkr1727@gmail.com>
2024-05-16 12:30:44 +0530

    Update main.py changed COPY line

--- python_fastapi/dpod-poc/app/main.py
+++ python_fastapi/dpod-poc/app/main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI, status, HTTPException, Response
 from pydantic import BaseModel
 from typing import Optional
+import docker
+import subprocess
 
 
 app = FastAPI()
 
+class ImageRegistry(BaseModel):
+    registry_url: str
+    userid: str
+    password: str
+
+sign_key = {
+    "slotName": "pkcs11:token=openssl_sign_DPOD",
+    "slotId": "slot-id=3",
+    "id": "id=%70%83%8e%fd%71%63%43%24%6b%cc%36%eb%82%b9%0e%bc%c2%cb%80%8f",
+    "object": "object=Cloud-Insights-Docker?module-path=/root/HSM_DPOD/libs/64/libCryptoki2.so",
+    "pinValue": "&pin-value=NewCOPass"
+}
 class Image(BaseModel):
     image_url: str
-    sign_key: str
+    sign_key: dict
     requestor: str = None
     team: Optional[str] = None
 
@@ -15,15 +29,30 @@ class Image(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/registry/login")
+def login_to_registry(registry: ImageRegistry):
+    # Login to the registry
+
+    client = docker.from_env()
+    try:
+        client.login(username=registry.userid, password=registry.password, registry=registry.registry_url)
+    except docker.errors.APIError as e:
+        return {"message": "Failed to login to registry", "error": str(e)}
+
+    # Call the function
+    login_to_registry(registry)
+    return {"message": "Login to registry successful"}
+
 @app.post("/sign/image/", status_code=status.HTTP_201_CREATED)
 def sign_image(payload: Image):
     print(payload)
-    #return Response(status_code=status.HTTP_201_CREATED)
+    signing_key = sign_key["slotName"] + ';' + sign_key["slotId"] + ';' + sign_key["id"] + ';' + sign_key["object"] + sign_key["pinValue"]
+    cosign_command = "cosign sign -key " + signing_key + " " + payload.image_url
+    # execute the cosign_command
+    try:
+        subprocess.run(cosign_command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        return {"message": "Failed to sign image", "error": str(e)}
     return {"message": "Image signed successfully",
             "image name": f"signed image {payload.image_url}"}
 
-@app.get("/image/{image_id}")
-def read_image(image_id: int, q: Optional[str] = None):
-    if image_id == 1:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Image not found")
-    return {"image_id": image_id, "q": q}
