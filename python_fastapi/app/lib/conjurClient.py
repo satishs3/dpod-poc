85044f4437f8b87f6d63bcd89ba7dbaf43b51579
satish3 <satish3@netapp.com>
2024-05-29 14:48:37 +0530

    modified conjurClient

--- python_fastapi/app/lib/conjurClient.py
+++ python_fastapi/app/lib/conjurClient.py
@@ -1,24 +1,22 @@
 import os
-from app.logger import logger
+import base64
 from conjur import Client
+from app.logger import logger
 from kubernetesClient import KubernetesClient
 
 # Conjur variables
 CONJUR_ACCOUNT = 'prod'
 
 CONJUR_SAFE_PARAMS = {
-    'production':  {'safe': 'D-CJR-SUM-ENG-S-DAAS',
-                    'login_id': 'host/prodvault/devops/D-CJR-SUM-ENG-S-DAAS/apps/host1',
+    'production':  {'safe': 'S-CJR-DPO-O-DPO',
+                    'login_id': 'host/prodvault/devops/S-CJR-DPO-O-DPO/DPO-host1',
                     'api_key_secret': 'conjur-api-key'},
 }
 
 CONJUR_CONNECT_PARAMS = {
-    'CORP': {'url': 'https://follower-west.corp.netapp.com',
+    'CORP': {'url': 'https://svc-pam.cls.eng.netapp.com',
              'pem_secret_name': 'conjur-corp-pem',
-             'pem_file': '/dpod/conjur-corp.pem'},
-    'ENG': {'url': 'https://svc-pam.cls.eng.netapp.com',
-            'pem_secret_name': 'conjur-eng-pem',
-            'pem_file': '/dpod/conjur-eng.pem'},
+             'pem_file': '/dpod/conjur.pem'},
 }
 
 class ConjurClient:
@@ -54,40 +52,33 @@ class ConjurClient:
                 logger.debug("Writing secret's password to pem file %s" % pem_file)
                 pem_fp.write(kube_secret['password'])
         return pem_file
-    
+
     def get_secret(self):
         '''
         Get secret from conjur safe
         '''
-        logger.info("Get secret from conjur safe'%s'")
+        logger.debug("Get secret from conjur safe")
         safe = CONJUR_SAFE_PARAMS[self.environment]['safe']
         login_id = CONJUR_SAFE_PARAMS[self.environment]['login_id']
-        conjur_api_key = self.get_conjur_api_key()
-
+        conjur_api_key = base64.b64decode(self.get_conjur_api_key())
         secret_value = None
-        # We don't know whether we're in 'CORP' or 'ENG' network, so we'll loop through each and
-        # try connecting and fetching the secret until successful
-        for environment in ["ENG", "CORP"]:
+        for environment in ["CORP"]:
             conjur_url = CONJUR_CONNECT_PARAMS[environment]['url']
             conjur_pem_file = self.get_conjur_pem_file(environment)
-
             try:
-                logger.debug("Try fetching secret from network '%s' for cluster '%s'"
-                                 % (environment, cluster))
                 client = Client(account=CONJUR_ACCOUNT,
                                 api_key=conjur_api_key,
                                 ca_bundle=conjur_pem_file,
-                                debug=False,
+                                debug=True,
                                 http_debug=False,
                                 login_id=login_id,
-
                                 password=None,
                                 ssl_verify=False,
                                 url=conjur_url)
 
                 # Get the <variable_id> used to fetch the secret value
                 #   prodvault/devops/<safe>/Operating System-SSHKeyVaulting-kubeconfig-<cluster>/password
-                variable_id = "prodvault/devops/%s/Operating System-SSHKeyVaulting-kubeconfig-%s/password" % (safe, cluster)
+                variable_id = "prodvault/devops/S-CJR-DPO-O-DPO/Misc-GenericPasswordVault-TestConjur-TestConjur/password"
 
                 # Fetch secret value for <variable_id>
                 secret_value = client.get(variable_id)
@@ -100,13 +91,12 @@ class ConjurClient:
 
             except Exception as e:
                 # Continue and try the next network
-                logger.debug("Fetching conjur secret from network '%s' for cluster '%s' failed"
-                                 % (environment, cluster))
+                logger.exception("Fetching conjur secret from network failed ->" + str(e))
                 continue
 
         return secret_value
-    
+
 if __name__ == "__main__":
     conjur_client = ConjurClient()
     secret = conjur_client.get_secret()
-    print(secret)
+    logger.debug(secret)
